899b0d2484559c354a2e39fee2d732595f460b8f
HaJisoo000 <somia00@idrsystems.co.kr>
2022-01-26 17:59:07 +0900

    베스트 상품의 상품리스트 2개단 수정, 교환 반품 padding 수정, 그외 css

--- study/morningdew/src/js/main.js
+++ study/morningdew/src/js/main.js
@@ -112,28 +112,3 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
-
-// // swiper 위치 수정값
-// jQuery(document).ready(function() { 
-//   var main_W = jQuery(".main-slide").width();
-//   if(main_W <= 1280){
-//     $('.main-slide .swiper-button-next').css('right','120px');
-//     $('.main-slide .swiper-button-prev-').css('left','120px');
-//   }else{
-//     var w_change = (main_W - 1280)/2;
-//     $('.main-slide .swiper-button-next').css('right',120+ w_change + 'px');
-//     $('.main-slide .swiper-button-prev').css('left',120+ w_change + 'px');
-//   }		
-
-//   $(window).resize(function(){
-//     var main_W = jQuery(".main-slide").width();
-//     if(main_W <= 1280){
-//       $('.main-slide .swiper-button-next').css('right','10px');
-//       $('.main-slide .swiper-button-prev').css('left','10px');
-//     }else{
-//       var w_change = (main_W - 1280)/2;
-//       $('.main-slide .swiper-button-next').css('right',10+ w_change + 'px');
-//       $('.main-slide .swiper-button-prev').css('left',10+ w_change + 'px');
-//     }
-//   });
-// }); //지우지마
