4a180461e6d855bf49ea111c737d06c3ff00ac8b
HaJisoo000 <somia00@idrsystems.co.kr>
2022-02-08 18:00:33 +0900

    main btn css 작업

---
+++ study/print_template/asset/js/custom.js
@@ -0,0 +1,11 @@
+var taxBtn = document.querySelector(".btn-tax-open");
+var cashBtn = document.querySelector(".btn-cash-open");
+var close = document.querySelector(".close");
+
+function popupOpen (){
+
+};
+
+function popupClose () {
+
+};
