dd943b372595747e19b48ec17a3867e7903c13bc
HaJisoo000 <somia00@idrsystems.co.kr>
2022-02-11 17:50:35 +0900

    영수증 페이지 작업 완료 - 현금영수증 마크업, popup js 오류 수정, 프린트 해보기

--- study/print_template/asset/js/custom.js
+++ study/print_template/asset/js/custom.js
@@ -1,28 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() { 
-
-  var target = document.querySelectorAll('.btn-open');
-  var popClose = document.querySelectorAll('.popup .btn-close')
-  var targetID;
+var target = document.querySelectorAll('.btn-open');
+var targetID;
   
-  // 팝업 열기
+// 팝업 열기
   for(var i = 0; i < target.length; i++){
     target[i].addEventListener('click', function(){
     // getAttribute : 선택한 요소의 특정 속성을 가져옴 ex) a값의 href
       targetID = this.getAttribute('href');
       document.querySelector(targetID).classList.remove("dn");
-      document.querySelector(targetID).classList.add("db");
     });
-  }
-  
-  // 팝업 닫기
-  for(var j = 0; j < target.length; j++){
-    popClose[j].addEventListener('click', function(){
-      document.querySelector(targetID).classList.remove("db");
-      document.querySelector(targetID).classList.add("dn");
-    });
-  }
-});
+  };
 
+// 팝업 닫기
+function popClose() {
+  // targetID -> a href의 #popTax와 #popCash에 클래스를 더함 
+  document.querySelector(targetID).classList.add("dn");
+};
 
 // print
 function pageprint()
@@ -30,4 +22,5 @@ function pageprint()
     window.print();
 }
 
+// 질문할거 
 
