8621bcbe6ffd82553c5641eb73e8c3f23ed695b6
HaJisoo000 <somia00@idrsystems.co.kr>
2022-05-04 17:51:36 +0900

    js 수정, 태블릿 레이아웃 수정

--- study/yeyak_seoul/assets/js/main.js
+++ study/yeyak_seoul/assets/js/main.js
@@ -1,19 +1,39 @@
 'use strict';
 
-var dept = document.querySelectorAll('.dept-list');
+var dept = document.querySelector('.dept-list');
 var subDept = document.querySelectorAll('.sub-dept');
+console.log(subDept); // function test() {
+//     for ( var i = 0; i < dept.length; i++ ) {
+//         dept[i].addEventListener('mouseover', () => {
+//             subDept[i].classList.add('h310')
+//         });
+//         dept[i].addEventListener('mouseout', () => {
+//             subDept[i].classList.remove('h310')
+//         })
+//     }
+// }
+// subDept 안되는 이유 찾기
 
-function dropdown(i) {
+var _loop = function _loop(i) {
   dept[i].addEventListener('mouseover', function () {
     subDept[i].classList.add('h310');
   });
-  dept[i].addEventListener('mouseout', function () {
-    subDept[i].classList.remove('h310');
-  });
+};
+
+for (var i = 0; i < dept.length; i++) {
+  _loop(i);
 }
 
-;
-
-for (var i = 0; i < 6; i++) {
-  dropdown(i);
-}
+for (var index = 0; index < array.length; index++) {
+  var element = array[index];
+} // function dropdown(i) {
+//     dept[i].addEventListener('mouseover', () => {
+//         subDept[i].classList.add('h310')
+//     });
+//     dept[i].addEventListener('mouseout', () => {
+//         subDept[i].classList.remove('h310')
+//     })
+// };
+// for (var i = 0; i <6; i++) {
+//     dropdown(i); 
+// }
--- study/yeyak_seoul/dev/js_dev/main.js
+++ study/yeyak_seoul/dev/js_dev/main.js
@@ -1,18 +1,47 @@
 'use strict';
 
-const dept = document.querySelectorAll('.dept-list')
-const subDept = document.querySelectorAll('.sub-dept')
+    const dept = document.querySelector('.dept-list');
+    const subDept = document.querySelectorAll('.sub-dept');
 
-function dropdown(i) {
-    dept[i].addEventListener('mouseover', () => {
-        subDept[i].classList.add('h310')
-    });
+    console.log(subDept);
 
-    dept[i].addEventListener('mouseout', () => {
-        subDept[i].classList.remove('h310')
-    })
-};
+    // function test() {
 
-for (var i = 0; i <6; i++) {
-    dropdown(i); 
-}
+    //     for ( var i = 0; i < dept.length; i++ ) {
+
+    //         dept[i].addEventListener('mouseover', () => {
+    //             subDept[i].classList.add('h310')
+    //         });
+
+    //         dept[i].addEventListener('mouseout', () => {
+    //             subDept[i].classList.remove('h310')
+    //         })
+    //     }
+    // }
+    
+    // subDept 안되는 이유 찾기
+
+    for ( let i=0; i<dept.length; i++ ) {
+        dept[i].addEventListener('mouseover', () => {
+            subDept[i].classList.add('h310')
+        });
+    }
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+
+// function dropdown(i) {
+//     dept[i].addEventListener('mouseover', () => {
+//         subDept[i].classList.add('h310')
+//     });
+
+//     dept[i].addEventListener('mouseout', () => {
+//         subDept[i].classList.remove('h310')
+//     })
+// };
+
+// for (var i = 0; i <6; i++) {
+//     dropdown(i); 
+// }
